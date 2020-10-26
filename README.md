090dbe8863ee57657235c55f62ec59b1d3d18530
lvemavarapu <lvemavarapu@gmail.com>
2020-10-27 06:56:35 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Clock app
+
+This application is used in the React lesson on class components, state, and state management.
---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Clock from "./Clock"
+
+const App = () => {
+	return <Clock date={new Date()} />
+}
+
+export default App

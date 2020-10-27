0f481fe6bce8f7d4c50800ca7ac5796d5a818ec1
Thomas Fisher <thomasjfisher1@gmail.com>
2020-10-27 11:25:27 +1100

    React clock app class work

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,32 +1,39 @@
 import React from "react"
 import "./Clock.css"
 
+function getIcon(season) {
+	let icon = `${season}.png`
+	return icon
+}
+
 const Clock = props => {
-	const { date } = props
+	const { date, season } = props
 	const seconds = date.getSeconds()
 	const minutes = date.getMinutes()
 	const hours = date.getHours()
+	const icon = getIcon(season)
 
 	return (
-		<div className="container">
-			<div className="clock-face">
-				<div className="clock">
-					<div className="hours-container">
-						<div
-							className="hours"
-							style={{ transform: `rotateZ(${hours * 30 + minutes / 2}deg)` }}
-						></div>
-					</div>
-					<div className="minutes-container">
-						<div className="minutes" style={{ transform: `rotateZ(${minutes * 6}deg)` }}></div>
-					</div>
-					<div className="seconds-container">
-						<div className="seconds" style={{ transform: `rotateZ(${seconds * 6}deg)` }}></div>
+		<div>
+			<div className="container">
+				<div className="clock-face" style={ { backgroundImage: `url(/${icon})` } }>
+					<div className="clock">
+						<div className="hours-container">
+							<div
+								className="hours"
+								style={ { transform: `rotateZ(${hours * 30 + minutes / 2}deg)` } }
+							></div>
+						</div>
+						<div className="minutes-container">
+							<div className="minutes" style={ { transform: `rotateZ(${minutes * 6}deg)` } }></div>
+						</div>
+						<div className="seconds-container">
+							<div className="seconds" style={ { transform: `rotateZ(${seconds * 6}deg)` } }></div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	)
 }
-
 export default Clock
